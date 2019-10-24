c076f888de9ba6dd825d85bb946b091af28a4e8c
Ryan Wolfe <rwolfe22@gmail.com>
2019-10-24 00:44:53 -0700

    Updates

---
+++ precache-manifest.10580b9e0b928dff51088dfec972cac1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57efdc06da591f47406a3ada5958e19b",
+    "url": "/modsy-api/index.html"
+  },
+  {
+    "revision": "be5bcbbdb6db3ffe72fb",
+    "url": "/modsy-api/static/css/main.eee4566c.chunk.css"
+  },
+  {
+    "revision": "fa29a994c0c2716426b3",
+    "url": "/modsy-api/static/js/2.a6841ff0.chunk.js"
+  },
+  {
+    "revision": "be5bcbbdb6db3ffe72fb",
+    "url": "/modsy-api/static/js/main.7bb77cfc.chunk.js"
+  },
+  {
+    "revision": "3100ea9f100e0e7d70fc",
+    "url": "/modsy-api/static/js/runtime-main.79021588.js"
+  },
+  {
+    "revision": "1d79418fdca19164459ba726aecba4dc",
+    "url": "/modsy-api/static/media/left-chevron.1d79418f.svg"
+  },
+  {
+    "revision": "0a607ec2b1805d6ab361e309ca53a5c9",
+    "url": "/modsy-api/static/media/question.0a607ec2.svg"
+  },
+  {
+    "revision": "c46d3e9497f10d90f4ea75831d78f27d",
+    "url": "/modsy-api/static/media/right-chevron.c46d3e94.svg"
+  }
+]);
